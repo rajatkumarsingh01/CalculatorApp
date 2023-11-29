93c979ebb2280e898bf9c6490b46a01e82ee1254
rajatkumarsingh01 <singhrajatkumar071@gmail.com>
2023-11-29 22:23:53 +0530

    [FIX] Append bug

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -11,16 +11,20 @@ import com.example.calculatorapp.databinding.ActivityMainBinding
 import kotlin.math.PI
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var displayTextView: TextView
 
     private lateinit var digitButtons: List<Button>
     private var currentNumber: Double = 0.0
     private var pendingOperator: String = ""
-   // private var newNumber: String = ""
     private var resultDisplayed: Boolean = false
-    private var flag : Int = 0
+    private var flag: Int = 0
 
+    companion object {
+        private const val KEY_CURRENT_NUMBER = "current_number"
+        private const val KEY_PENDING_OPERATOR = "pending_operator"
+        private const val KEY_RESULT_DISPLAYED = "result_displayed"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +33,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         displayTextView = findViewById(R.id.displayTextView)
 
+        if (savedInstanceState != null) {
+            // Restore state
+            currentNumber = savedInstanceState.getDouble(KEY_CURRENT_NUMBER, 0.0)
+            pendingOperator = savedInstanceState.getString(KEY_PENDING_OPERATOR, "")
+            resultDisplayed = savedInstanceState.getBoolean(KEY_RESULT_DISPLAYED, false)
 
-
-
-        binding.buttonMore.setOnClickListener {
-            val intent=Intent(this,MoreMathsActivity::class.java)
-            startActivity(intent)
+            // Update UI with the restored state
+            displayTextView.text = currentNumber.toString()
         }
 
-
+        binding.buttonMore.setOnClickListener {
+            val intent = Intent(this, MoreMathsActivity::class.java)
+            startActivity(intent)
+        }
 
         digitButtons = listOf(
             binding.button0,
@@ -55,13 +64,14 @@ class MainActivity : AppCompatActivity() {
         for (button in digitButtons) {
             button.setOnClickListener {
                 val digit = button.text.toString()
-                Log.d("main","digit typed  : "+digit.toString())
+
                 appendDigitToTextView(digit)
-                if(flag==0){
-                    currentNumber = digit.toDouble()
+                if (flag == 0) {
+                    currentNumber = displayTextView.text.toString().toDouble()
                 }
             }
         }
+
         binding.apply {
             buttonDot.setOnClickListener {
                 appendDecimalToTextView()
@@ -96,30 +106,25 @@ class MainActivity : AppCompatActivity() {
             }
 
             buttonPI.setOnClickListener {
-
                 calculatePI()
             }
         }
-
-
-
     }
 
     private fun calculatePI() {
-            val result = 3.14159
-            displayTextView.text = result.toString()
-            currentNumber = result
+        val result = PI
+        displayTextView.text = result.toString()
+        currentNumber = result
     }
 
     private fun calculatePercentage() {
         val currentText = displayTextView.text.toString()
         val value = currentText.toDouble()
         // Perform the percentage calculation (divide by 100)
-        val result = value/ 100.0
+        val result = value / 100.0
         // Update the display with the result
         displayTextView.text = result.toString()
-        currentNumber=result
-
+        currentNumber = result
     }
 
     private fun appendDecimalToTextView() {
@@ -134,39 +139,45 @@ class MainActivity : AppCompatActivity() {
             displayTextView.text = newText
             resultDisplayed = false
         }
-
     }
 
     private fun appendDigitToTextView(digit: String) {
         val currentText = displayTextView.text.toString()
-        val newText = if (currentText == "0") {
-            digit
-        } else {
-            currentText + digit
-        }
-        displayTextView.text = newText
-         resultDisplayed = false
 
+        if (resultDisplayed) {
+            // If a result is displayed, start a new number
+            displayTextView.text = digit
+            resultDisplayed = false
+        } else {
+            // If the current text is "0" or an operator is pending, replace it with the new digit
+            if(currentText != "0"){
+                displayTextView.text = currentText + digit
+
+            }
+            else if (currentText == "0" || pendingOperator.isNotEmpty()) {
+                displayTextView.text = digit
+            } else {
+                // Otherwise, append the new digit to the existing text
+                displayTextView.text = currentText + digit
+            }
+        }
     }
+
     private fun setOperator(operator: String) {
         calculateResult()
         pendingOperator = operator
-       // newNumber = displayTextView.text.toString()
-        displayTextView.text ="0"
+        //displayTextView.text = "0"
     }
-
-
 
     private fun calculateResult() {
         flag = 1
         val secondNumber = displayTextView.text.toString().toDouble()
-        Log.d("main","calculated ")
-        when (pendingOperator) {
 
+        Log.d("mainii", secondNumber.toString())
+        when (pendingOperator) {
             "+" -> currentNumber += secondNumber
             "-" -> currentNumber -= secondNumber
             "*" -> currentNumber *= secondNumber
-
             "/" -> {
                 if (secondNumber != 0.0) {
                     currentNumber /= secondNumber
@@ -174,43 +185,33 @@ class MainActivity : AppCompatActivity() {
                     displayTextView.text = "Error"
                     return
                 }
-
             }
         }
-            displayTextView.text = currentNumber.toString()
-                  // newNumber = ""
-                resultDisplayed = true
-
-        }
-
-     private fun clearAll() {
-            flag = 0
-            currentNumber = 0.0
-            pendingOperator = ""
-          //  newNumber = ""
-            displayTextView.text = "0"
-        }
-
-
-
+        displayTextView.text = currentNumber.toString()
+        resultDisplayed = true
     }
 
+    private fun clearAll() {
+        flag = 0
+        currentNumber = 0.0
+        pendingOperator = ""
+        displayTextView.text = "0"
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Save the current state
+        outState.putDouble(KEY_CURRENT_NUMBER, currentNumber)
+        outState.putString(KEY_PENDING_OPERATOR, pendingOperator)
+        outState.putBoolean(KEY_RESULT_DISPLAYED, resultDisplayed)
 
+        // Call the superclass to save any view hierarchy
+        super.onSaveInstanceState(outState)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // Restore any additional UI state if needed
+        // For example, if you have additional UI components, you can restore their state here
+    }
+}
